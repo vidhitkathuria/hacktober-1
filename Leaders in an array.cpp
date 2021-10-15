e7cabfd4fc3fbea22e86b4b15d58056ec35abcba
Aviral Bansal <56379558+aviral-bansal@users.noreply.github.com>
2021-10-15 22:38:22 +0530

    Update Leaders in an array.cpp

--- Leaders in an array.cpp
+++ Leaders in an array.cpp
@@ -36,4 +36,34 @@ Expected Auxiliary Space: O(n)
 
 Constraints:
 1 <= n <= 107
-0 <= Ai <= 107 /*
+0 <= Ai <= 107 
+*/
+
+#include<iostream>
+using namespace std;
+
+/*C++ Function to print leaders in an array */
+void printLeaders(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int j;
+		for (j = i+1; j < size; j++)
+		{
+			if (arr[i] <=arr[j])
+				break;
+		}
+		if (j == size) // the loop didn't break
+			cout << arr[i] << " ";
+}
+}
+
+/* Driver program to test above function */
+int main()
+{
+	int arr[] = {16, 17, 4, 3, 5, 2};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	printLeaders(arr, n);
+	return 0;
+}
+
